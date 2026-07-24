ed9dcc7ad1e678549eef158581e4e890bcda8819
Vana-dot <geovanavenancio5556@gmail.com>
2026-07-24 18:45:13 -0300

    Atualizações

--- algoritmos/floyd_warshall.py
+++ algoritmos/floyd_warshall.py
@@ -1,15 +1,44 @@
-#Algoritmo de Floyd-Warshall - menor caminho entre todos os pares de vertices.
+from __future__ import annotations
+
+from math import inf
+
+from algoritmos.comum import Aresta, MatrizDistancias, validar_grafo
 
 
-def calcular(grafo):
+def floyd_warshall(
+    numero_vertices: int,
+    arestas: list[Aresta],
+) -> MatrizDistancias:
+    validar_grafo(numero_vertices, arestas)
 
-    n = len(grafo)
-    dist = [linha[:] for linha in grafo]
+    distancias: MatrizDistancias = [
+        [inf] * numero_vertices for _ in range(numero_vertices)
+    ]
 
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                if dist[i][k] + dist[k][j] < dist[i][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
+    for i in range(numero_vertices):
+        distancias[i][i] = 0
 
-    return dist
+    for u, v, peso in arestas:
+        if peso < distancias[u][v]:
+            distancias[u][v] = peso
+
+    for k in range(numero_vertices):
+        for i in range(numero_vertices):
+            distancia_ik = distancias[i][k]
+            if distancia_ik == inf:
+                continue
+
+            for j in range(numero_vertices):
+                distancia_kj = distancias[k][j]
+                if distancia_kj == inf:
+                    continue
+
+                nova_distancia = distancia_ik + distancia_kj
+                if nova_distancia < distancias[i][j]:
+                    distancias[i][j] = nova_distancia
+
+    for i in range(numero_vertices):
+        if distancias[i][i] < 0:
+            raise ValueError("O grafo possui ciclo de peso negativo.")
+
+    return distancias
